450d3b3d1f1cd39c963909a0d95b21da6ea6608b
daryan-kf <daryankamalifar@gmail.com>
2025-03-18 22:45:25 +0100

    Add User model with Sequelize integration for user management, including fields for id, username, email, and password.

---
+++ src/models/User.ts
@@ -0,0 +1,44 @@
+import {DataTypes, Model} from "sequelize";
+import sequelize from "../config/database";
+
+class User extends Model {
+  public id!: number;
+  public username!: string;
+  public email!: string;
+  public password!: string;
+  public createdAt!: Date;
+  public updatedAt!: Date;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "users",
+  }
+);
+
+export default User;
