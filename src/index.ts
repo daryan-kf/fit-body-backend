a0139493cb7fa6b9ea48ec107d28db8e8fe99b22
daryan-kf <daryankamalifar@gmail.com>
2025-03-19 14:46:46 +0100

    Refactor server setup in index.ts to use user schema and resolvers, implement auth middleware, and improve code clarity with consistent single quotes.

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,23 @@
-import {ApolloServer} from "@apollo/server";
-import {startStandaloneServer} from "@apollo/server/standalone";
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 
-const typeDefs = `
- type Book {
-    title: String
-    author: String
-  }
+import { authMiddleware } from '@/middlewares/authMiddleware';
 
-   type Query {
-    books: [Book]
-  }
-`;
+import userSchema from '@/schema/userSchema';
+import userResolvers from '@/resolvers/userResolvers';
 
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs: [userSchema],
+    resolvers: [userResolvers],
+  });
 
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+    context: async ({ req }) => authMiddleware({ req }),
+  });
+
+  console.log(`🚀  Server ready at: ${url}`);
 };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-const {url} = await startStandaloneServer(server, {
-  listen: {port: 4000},
-});
-
-console.log(`🚀  Server ready at: ${url}`);
+startServer();
